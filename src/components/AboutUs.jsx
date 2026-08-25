7a020cd1a092b2f06fa674c5a7922bb320121328
Logith-VL <logith.vl@neurealm.com>
2026-08-25 22:59:56 +0530

    Create AboutUs component for Paradise Nursery
    Added AboutUs component with description of Paradise Nursery.

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,43 @@
+function AboutUs() {
+
+  return (
+
+    <section className="about-section">
+
+      <div className="about-content">
+
+        <h2>About Paradise Nursery</h2>
+        
+        <p>
+
+          Paradise Nursery is an online plant shop dedicated to bringing
+
+          beautiful and healthy plants into your home. We offer a carefully
+
+          selected collection of houseplants suitable for different spaces
+
+          and lifestyles.
+
+        </p>
+        
+        <p>
+
+          From aromatic, flowering and indoor plants to air-purifying varieties,
+
+          our goal is to make it easy for everyone to enjoy the beauty and
+
+          benefits of nature indoors.
+
+        </p>
+
+      </div>
+
+    </section>
+
+  );
+
+}
+
+ 
+
+export default AboutUs;
